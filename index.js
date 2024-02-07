fe7b0b0ae7707638e7171b28a316b6f44be94242
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-02-07 10:34:20 +0800

    add locations file add get request for locations
    add main.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const locations = require('./locations')
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
 
 app.use(express.static('src'))
+
+app.get('/locations', (req, res) => {
+  res.json(locations)
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
